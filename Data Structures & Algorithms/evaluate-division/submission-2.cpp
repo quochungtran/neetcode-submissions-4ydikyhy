dfd2898d75442dc0f20b470db2a7b1a6c14dd401
TRAN Quoc Hung <71230286+quochungtran@users.noreply.github.com>
2026-04-04 23:46:33 +0800

    Add: evaluate-division - submission-2

---
+++ Data Structures & Algorithms/evaluate-division/submission-2.cpp
@@ -0,0 +1,72 @@
+class Solution {
+    unordered_map<string, vector<pair<string, double>>> adj;
+    unordered_set<string> visited;
+
+public:
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        // build adj list
+        int n = equations.size();
+        for (int i = 0; i < n; i++){
+            string firstStr = equations[i][0];
+            string secondStr = equations[i][1];
+            auto weight = values[i];
+            adj[firstStr].push_back({secondStr, weight});
+            adj[secondStr].push_back({firstStr, 1.0/weight});
+        }
+        vector<double> res;
+        for (auto querie: queries){
+            auto src = querie[0];
+            auto des = querie[1];
+            res.push_back(dfs(src, des));
+            visited.clear();
+        }   
+
+        return res;
+    }
+    double dfs(string src, string des){
+        if (!adj.contains(src) || !adj.contains(des)){
+            return -1.0;
+        }
+
+        if (src == des){
+            return 1.0;
+        }
+        visited.insert(src);
+        for (const auto&[nei, weight]: adj[src]){
+            if (!visited.contains(nei)){
+                auto result = dfs(nei, des);
+                if (result != -1.0){
+                    return weight * result;
+                }
+            }
+        }
+        return -1;
+    }
+    double bfs(string src, string des){
+        if (!adj.contains(src) || !adj.contains(des)){
+            return -1.0;
+        }
+
+        queue<pair<string, double>> q;
+
+        q.push({src, 1.0});
+        visited.insert(src);
+
+        while(!q.empty()){
+            auto [curr, curr_wei] = q.front();
+            q.pop();
+
+            if (curr == des){
+                return curr_wei;
+            }
+            for (auto [nei, w]: adj[curr]){
+                if (!visited.contains(nei))
+                {
+                    q.push({nei, curr_wei * w});
+                    visited.insert(nei);
+                }
+            }
+        }
+        return -1.0;
+    }
+};
