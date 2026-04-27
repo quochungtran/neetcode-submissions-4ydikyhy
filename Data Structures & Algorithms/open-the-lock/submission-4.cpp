f8d22d2be87d7f5828b78ef05b035d670a5116ab
TRAN Quoc Hung <71230286+quochungtran@users.noreply.github.com>
2026-04-27 17:06:58 +0800

    Add: open-the-lock - submission-4

---
+++ Data Structures & Algorithms/open-the-lock/submission-4.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int openLock(vector<string>& deadends, string target) {
+        // BFS running from 0000 state to target 
+        // we turn or deadends 0000, target to nodes of a graph
+        // when we reach to target we return the minimum steps
+
+        unordered_set<string> deadEnds_set(deadends.begin(), deadends.end());
+        if (deadEnds_set.contains("0000")) return -1;
+
+        queue<string> q;
+        q.push("0000");
+        unordered_set<string> visited;  
+        visited.insert("0000");
+
+
+        int times = 0;
+
+        while(!q.empty()){            
+            const auto qSize = q.size();
+            for (int i = 0; i < qSize; i++){
+                auto curr = q.front();
+                q.pop();
+
+                if (curr == target)
+                    return times;
+                
+                for (auto nextState: getNextState(curr)){
+                    if (!visited.contains(nextState) && !deadEnds_set.contains(nextState)){
+                        visited.insert(nextState);
+                        q.push(nextState);
+                    }
+                }
+            }
+            times++;
+        }
+
+        return -1;
+    }
+
+    vector<string> getNextState(const string& curr){
+        vector<string> res;
+        res.reserve(8);
+        
+        for (int i = 0; i < 4; i++){
+            const int digit = curr[i] - '0';
+
+            auto nextStage = [&](int delta){
+                string next = curr;
+                next[i] = '0' + (digit + delta + 10)%10;
+                return next; 
+            };
+
+            res.push_back(nextStage(1));
+            res.push_back(nextStage(-1));
+        }
+
+        return res;
+    }
+};
