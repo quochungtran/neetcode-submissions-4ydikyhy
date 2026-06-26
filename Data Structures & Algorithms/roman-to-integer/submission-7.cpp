78f5ebd4125b7e3348e03e8b060f7967ec9d2452
TRAN Quoc Hung <71230286+quochungtran@users.noreply.github.com>
2026-06-26 08:42:44 +0200

    Add: roman-to-integer - submission-7

---
+++ Data Structures & Algorithms/roman-to-integer/submission-7.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        // hashmap sympol -> value int
+        unordered_map<string, int> symPolToInterger = {
+            {"I", 1},
+            {"V", 5},
+            {"X", 10},
+            {"L", 50},
+            {"C", 100},
+            {"D", 500},
+            {"M", 1000},
+            {"IV", 4},
+            {"IX", 9},
+            {"XL", 40},
+            {"XC", 90},
+            {"CD", 400},
+            {"CM", 900}
+        };
+        // interation of s
+        int index = 0;
+        int res = 0;
+        while (index < s.size()){
+            if (index + 1 < s.size() && symPolToInterger.contains(s.substr(index, 2))){
+                res = res + symPolToInterger[s.substr(index, 2)];
+                index = index + 2;
+            }
+            else{
+                res = res + symPolToInterger[s.substr(index, 1)];
+                index++;
+            }
+        }// IV
+         // res -1 + 5 = 4
+        return res;
+    }
+};
