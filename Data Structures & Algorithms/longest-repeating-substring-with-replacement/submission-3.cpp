477b3cf3ead5b3d50e60f89911617a30ba9facee
TRAN Quoc Hung <71230286+quochungtran@users.noreply.github.com>
2026-06-26 09:34:25 +0200

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        // condition to create a sub string with repeated characters:
+        // length(s) - max_freq_of_any_char(substring) < k 
+        // -> track the current of length of substr
+
+        unordered_map<char, int> freq;
+        int maxFreq = 0;
+        int r = 0;
+        int l = 0;
+        int result = 0;
+        int n = s.size();
+        while (r < n){
+            freq[s[r]]++;
+            // maxFreq
+            maxFreq = max(maxFreq, freq[s[r]]);
+            while (r - l + 1 - maxFreq > k){
+                freq[s[l]]--;
+                l++;
+            } 
+            result = max(result, r - l + 1);
+            r++;
+        }
+        return result;
+    }
+};
+
+// A A B A B B A
+
+// freq A: 1  B: 2 , k = 1
+// maxFreq = 2
+// result = 4
