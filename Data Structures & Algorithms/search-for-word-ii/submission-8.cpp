d3ba4b710b555e0459a181aefcbee288251ea11e
TRAN Quoc Hung <71230286+quochungtran@users.noreply.github.com>
2026-07-05 10:50:11 +0200

    Add: search-for-word-ii - submission-8

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-8.cpp
@@ -0,0 +1,59 @@
+struct TrieNode {
+    TrieNode* children[26] = {nullptr};
+    bool isWord = false;
+    void addWord(string word) {
+        TrieNode* curr = this;
+        for (char c : word) {
+            if (!curr->children[c - 'a']) curr->children[c - 'a'] = new TrieNode();
+            curr = curr->children[c - 'a'];
+        }
+        curr->isWord = true;
+    }
+};
+
+
+class Solution {
+    unordered_set<string> res;
+    int rows, cols;
+    int dirs[4][2] = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+public:
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+        TrieNode* root = new TrieNode();
+        rows = board.size();
+        cols = board[0].size();
+
+        for (auto& w: words){
+            root->addWord(w);
+        }
+
+        vector<vector<bool>> visited(rows, vector<bool>(cols, false));
+
+        for (int r = 0; r < rows; r++){
+            for (int c = 0; c < cols; c++){
+                if (root->children[board[r][c] - 'a'] != nullptr){
+                    dfs(r, c, "", board, root->children[board[r][c] - 'a'], visited);
+                }
+            }
+        }
+
+        return vector<string>(res.begin(), res.end());
+    }
+
+    void dfs(int r, int c, string currWord, const vector<vector<char>>& board, TrieNode* node, vector<vector<bool>>& visited){
+        currWord += board[r][c];
+        if (node->isWord){
+            res.insert(currWord);
+        }
+
+        visited[r][c] = true;
+        for (auto& dir: dirs){
+            int nr = dir[0] + r;
+            int nc = dir[1] + c;
+
+            if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && !visited[nr][nc] && node->children[board[nr][nc] - 'a']){
+                dfs(nr, nc, currWord, board, node->children[board[nr][nc] - 'a'], visited);
+            }
+        }
+        visited[r][c] = false;
+    }
+};
