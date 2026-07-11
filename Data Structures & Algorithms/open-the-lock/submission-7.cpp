ded660bd9715b0a7d902927c9acb3731e2f4176d
TRAN Quoc Hung <71230286+quochungtran@users.noreply.github.com>
2026-07-11 07:13:08 +0200

    Add: open-the-lock - submission-7

---
+++ Data Structures & Algorithms/open-the-lock/submission-7.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<string> getNextLock(string lock) {
+        vector<string> nexts;
+        for (int i = 0; i < 4; ++i) {
+            char c = lock[i];
+            lock[i] = (c == '9' ? '0' : c + 1);
+            nexts.push_back(lock);
+            lock[i] = (c == '0' ? '9' : c - 1);
+            nexts.push_back(lock);
+            lock[i] = c;
+        }
+        return nexts;
+    }
+
+    int openLock(vector<string>& deadends, string target) {
+        if (target == "0000") {
+            return 0;
+        }
+
+        unordered_set<string> visit(deadends.begin(), deadends.end());
+        if (visit.count("0000")) return -1;
+        
+        queue<string> q;
+        q.push("0000");
+        visit.insert("0000");
+        int steps = 0;
+        while (!q.empty()){
+            auto sizeQ = q.size();
+            for (int i = 0; i < sizeQ; i++){
+                auto curr = q.front();
+                q.pop();
+                if (curr == target){
+                    return steps;
+                }
+                for (const auto& nextLock: getNextLock(curr)){
+                    if (visit.count(nextLock)){
+                        continue;
+                    }
+                    visit.insert(nextLock);
+                    q.push(nextLock);
+                }
+            }
+            steps++;
+        }
+
+        return -1;
+    }
+};
